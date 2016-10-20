d9d71a09c8b0c7f03269a1d2ea1c36bdfaf41ed5
Facundo Maero <facundojmaero@gmail.com>
2016-10-20 19:01:29 -0300

    Agrego codigo Arduino

---
+++ Arduino/Serial-arduino.ino
@@ -0,0 +1,19 @@
+#include <SoftwareSerial.h>
+
+SoftwareSerial mySerial(10,11);
+
+void setup() {
+        Serial.begin(9600);    //9600 baudios
+        while(!Serial)
+        {;}
+        
+        mySerial.begin(9600);
+}
+
+void loop() {
+         if(mySerial.available()) {
+           int dato = mySerial.read();
+           dato = dato;
+		   Serial.println(dato);
+        }     
+}
